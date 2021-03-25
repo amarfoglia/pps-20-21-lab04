6162d09c52814fe21d997385214033fb5a788719
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-03-25 17:30:04 +0100

    implement and test Student creation through case class

--- test/u04lab/code/StudentTest.scala
+++ test/u04lab/code/StudentTest.scala
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
 import org.junit.jupiter.api.Test
 
 class StudentTest {
+  val student: Student = Student("alberto", 2021)
+  val course: Course = Course("PPS", "Viroli")
 
   @Test
   def testCourseApply(): Unit = {
-    val course: Course = Course("PPS", "m.viroli")
     assertEquals(s"CourseImpl(${course.name},${course.teacher})", course.toString)
+  }
+
+  @Test
+  def testStudentApply(): Unit = {
+    assertEquals(s"StudentImpl(${student.name},${student.year})", student.toString)
   }
 }
